94766658225afdecfaeac66ebee6d9545927032f
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-03 17:21:22 +0900

    day01_230203_setting

---
+++ src/main/java/com/sparta/hangheamemo/HangheamemoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hangheamemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangheamemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangheamemoApplication.class, args);
+    }
+
+}
