4f899da521a53d9622ebe2f2ac2ac51d59f5d861
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-03 23:03:41 +0900

    Day21_230203

---
+++ src/main/java/com/sparta/hangheamemo/Star.java
@@ -0,0 +1,13 @@
+package com.sparta.hangheamemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Star {
+    String name;
+    int age;
+}
